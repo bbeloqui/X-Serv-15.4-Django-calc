814b6c5f1cc01cce770eef6eb22e75a113815b1e
bbeloqui <bbeloqui@hotmail.com>
2016-03-19 19:00:15 +0100

    ejercicio

--- project/urls.py
+++ project/urls.py
@@ -5,6 +5,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'(\d+)\+(\d+)$', "calc.views.suma"),
+    url(r'(\d+)-(\d+)$', "calc.views.resta"),
+    url(r'(\d+)\*(\d+)$', "calc.views.multiplicar"),
+    url(r'(\d+)/(\d+)$', "calc.views.dividir"),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'.*', "calc.views.error"),
 )
